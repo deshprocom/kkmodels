4c18b1b671c6331c331cca179ab285556feb5fc1
Ricky <rickycxj@gmail.com>
2018-05-08 12:16:21 +0800

    完成手机号邮箱注册

--- user.rb
+++ user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   include UserFinders
+  include UserUniqueValidator
+  include UserNameGenerator
+  include UserCreator
   include UserCountable
+  mount_uploader :avatar, AvatarUploader
 
   has_one :counter, class_name: 'UserCounter', dependent: :destroy
+
+  def avatar_path
+    avatar.url.presence || wx_avatar
+  end
 end
