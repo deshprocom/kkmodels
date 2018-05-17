db5db1c317fc25b6dda171623a56e6353fe5f042
Ricky <rickycxj@gmail.com>
2018-05-17 11:53:46 +0800

    修改counter

--- concerns/topic_countable.rb
+++ concerns/topic_countable.rb
@@ -3,6 +3,6 @@ module TopicCountable
   included { after_create :create_topic_counter }
 
   def increase_page_views
-    counter.increment!(:page_views)
+    topic_counter.increment!(:page_views)
   end
 end
