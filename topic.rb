db709f73362da1692785649be5b9961df30e808e
Ricky <rickycxj@gmail.com>
2018-05-29 14:31:27 +0800

    只有审核失败的时候才不显示

--- topic.rb
+++ topic.rb
@@ -9,7 +9,7 @@ class Topic < ApplicationRecord
   has_one    :topic_counter, dependent: :destroy
   serialize :images, JSON
 
-  scope :user_visible, -> { where(status: 'passed') }
+  scope :user_visible, -> { where.not(status: 'failed') }
 
   enum body_type: { long: 'long', short: 'short' }
   enum status: { pending: 'pending', passed: 'passed', failed: 'failed' }
