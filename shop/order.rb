18ba1e5ec4efb4fabb09bcdb32408fd01a596aff
breeze-yang <breeze@deshpro.com>
2018-06-07 17:06:14 +0800

    修改order 的status

--- shop/order.rb
+++ shop/order.rb
@@ -15,9 +15,7 @@ module Shop
                    paid: 'paid',
                    delivered: 'delivered',
                    completed: 'completed',
-                   canceled: 'canceled',
-                   returning: 'returning',
-                   returned: 'returned' }
+                   canceled: 'canceled'}
 
     before_create do
       self.order_number = Services::UniqueNumberGenerator.call(Order)
