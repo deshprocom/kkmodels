b05fef7366ac19f8672e12ba15bdd63323d8b8c5
Ricky <rickycxj@gmail.com>
2018-06-08 12:31:25 +0800

    积分增加排序

--- integral_rule.rb
+++ integral_rule.rb
@@ -1,5 +1,10 @@
 class IntegralRule < ApplicationRecord
   mount_uploader :icon, ImageUploader
+  scope :position_desc, -> { order(position: :desc) }
+
+  before_create do
+    self.position = IntegralRule.position_desc.first&.position.to_i + 100000
+  end
 
   def icon_path
     icon.url.presence || ""
