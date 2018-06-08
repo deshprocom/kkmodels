5dba80573c56411aaa8c050872d313a56a9b56e0
Ricky <rickycxj@gmail.com>
2018-06-08 14:56:19 +0800

    积分规则整理

--- integral_rule.rb
+++ integral_rule.rb
@@ -1,6 +1,7 @@
 class IntegralRule < ApplicationRecord
   mount_uploader :icon, ImageUploader
   scope :position_asc, -> { order(position: :asc) }
+  scope :position_desc, -> { order(position: :desc) }
 
   before_create do
     self.position = IntegralRule.position_desc.first&.position.to_i + 100000
