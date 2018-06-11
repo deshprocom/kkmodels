949c9a676de3c7082fa07852613bec3508c5f3b0
Ricky <rickycxj@gmail.com>
2018-06-11 12:19:11 +0800

    添加热增长冷增长

--- topic.rb
+++ topic.rb
@@ -7,6 +7,7 @@ class Topic < ApplicationRecord
   has_many   :replies,   as: :target, dependent: :destroy
   has_many   :reports,   as: :target, dependent: :destroy
   has_one    :topic_counter, dependent: :destroy
+  has_one :view_toggle, as: :target, dependent: :destroy
   serialize :images, JSON
 
   scope :user_visible, -> { where.not(status: 'failed') }
@@ -27,6 +28,6 @@ class Topic < ApplicationRecord
   end
 
   def total_views
-    topic_counter.page_views
+    topic_counter.page_views + topic_counter.view_increment
   end
 end
