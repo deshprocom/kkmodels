627ef92a71b5da8fe25690a5a050680c79e61837
Ricky <rickycxj@gmail.com>
2018-06-26 15:09:15 +0800

    更新优惠券model

--- coupon_temp.rb
+++ coupon_temp.rb
@@ -1,2 +1,10 @@
 class CouponTemp < ApplicationRecord
+  has_many :coupons, dependent: :destroy
+
+  enum coupon_type: { hotel: 'hotel', shop: 'shop', new_user: 'new_user' }
+  enum discount_type: { reduce: 'reduce', full_reduce: 'full_reduce', rebate: 'rebate' }
+
+  def could_delete?
+    !coupons.where.not('receive_time IS NULL').exists?
+  end
 end
