735909c8653493844a28fdda110c3bce8418991f
Ricky <rickycxj@gmail.com>
2018-07-11 16:42:36 +0800

    发放奖励

--- user.rb
+++ user.rb
@@ -34,7 +34,11 @@ class User < ApplicationRecord
   action_store :follow,   :user,  counter_cache: 'followers_count', user_counter_cache: 'following_count'
 
   # 刷新访问时间
+  # 统计登录天数和连续登录天数
   def touch_visit!
+    interval_day = (Date.today - last_visit.to_date).to_i
+    increase_login_days if interval_day >= 1
+    interval_day.eql?(1) ? increase_continuous_login_days : clear_continuous_login_days
     self.last_visit = Time.zone.now
     save
   end
@@ -64,5 +68,10 @@ class User < ApplicationRecord
 
   def p_user
     user_relation.p_user.user
+  end
+  
+  def get_pocket_moneys
+    # 登录满7天并且分享次数大于2
+    new_user_register_award(self) if counter.login_days >= 7 && counter.share_count >= 2
   end
 end
