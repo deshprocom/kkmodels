6746083739f8430f92e69372fb3cf32ea089bff7
breeze-yang <breeze@deshpro.com>
2018-07-11 18:56:19 +0800

    完善room_price的功能

--- hotel_room_price.rb
+++ hotel_room_price.rb
@@ -1,10 +1,12 @@
 class HotelRoomPrice < ApplicationRecord
+  attr_accessor :dates # 用于activeadmin的方法 t.input(dates) views/admin/room_prices/_form.html.haml
+
   belongs_to :hotel_room, required: false
   belongs_to :hotel, required: false
-
   # wday is the day of week (0-6, Sunday is zero).
   WDAYS = %w[sun mon tue wed thu fri sat].freeze
   validates :wday, inclusion: { in: WDAYS }, allow_nil: true
+  validates :date, uniqueness: { scope: :hotel_room_id }, allow_nil: true
 
   after_initialize do
     self.date ||= Date.current unless is_master
