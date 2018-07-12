0ca80332bbad8d07a98b290dfde448cc5026d149
Ricky <rickycxj@gmail.com>
2018-07-12 13:28:56 +0800

    完成钱包提现接口

--- withdrawal.rb
+++ withdrawal.rb
@@ -4,7 +4,7 @@ class Withdrawal < ApplicationRecord
   # 计算用户单月提款成功金钱
   def self.month_success_withdraw(user)
     user.withdrawals
-        .where(option_status: 'success')
+        .where.not(option_status: 'failed')
         .where('created_at BETWEEN ? AND ?', Time.now.at_beginning_of_month, Time.now.at_end_of_month)
         .sum(:amount)
   end
