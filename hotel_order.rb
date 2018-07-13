0342cc52032f4a0c9e7e8cc599d1b2aea220ecf2
breeze-yang <breeze@deshpro.com>
2018-07-13 10:31:53 +0800

    修改hotel_order.rb

--- hotel_order.rb
+++ hotel_order.rb
@@ -76,8 +76,8 @@ class HotelOrder < ApplicationRecord
     OrderMailer.notify_staff_after_paid(self).deliver_later
   end
 
-  # "[澳门旅行]澳门金沙城中心假日酒店7月14日入住假日高级房2间1晚，已预定成功，入住人请携带有效证件及入境标签联系客服：#{ENV['HOTEL_STAFF_TEL']}办理入住。"
-  NOTICE_USER_AFTER_PAID_SMS = "[澳门旅行]预定成功:%s，入住人请携带有效证件及入境标签联系客服:#{ENV['HOTEL_STAFF_TEL']} 办理入住。"
+  # "[澳门旅行]预定成功:澳门金沙城中心假日酒店7月14日入住假日高级房2间1晚，入住人请携带有效证件及入境标签联系客服：#{ENV['HOTEL_STAFF_TEL']}办理入住。"
+  NOTICE_USER_AFTER_PAID_SMS = "[澳门旅行]预定成功:%s，入住人请携带有效证件及入境标签联系客服: #{ENV['HOTEL_STAFF_TEL']} 办理入住。"
   def notify_user_after_paid
     # 酒店订单支付后，短信通知用户
     date = checkin_date.strftime('%m月%d日')
