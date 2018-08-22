143e8472418fc47d0b77544d6878133bb5266a87
breeze-yang <breeze@deshpro.com>
2018-08-22 16:40:18 +0800

    增加回收汇率

--- exchange_rate.rb
+++ exchange_rate.rb
@@ -1,3 +1,3 @@
 class ExchangeRate < ApplicationRecord
-  enum rate_type: { real_time: 'real_time', local: 'local', recover: 'recover' }
+  enum rate_type: { real_time: 'real_time', local: 'local', receiving: 'receiving' }
 end
