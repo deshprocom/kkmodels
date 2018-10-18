cd3f0e011b5ab57178856382dc374f3fd4612e3e
Ricky <rickycxj@gmail.com>
2018-10-18 18:14:16 +0800

    更新活动model

--- activity.rb
+++ activity.rb
@@ -1,8 +1,19 @@
 class Activity < ApplicationRecord
   mount_uploader :banner, ImageUploader
+  include Publishable
+
+  validates :banner, presence: true, if: :new_record?
+
+  scope :published, -> { where(published: true) }
 
   after_initialize do
     self.begin_time ||= Date.current
     self.end_time ||= Date.current
+  end
+
+  def preview_image
+    return '' if banner&.url.nil?
+
+    banner.url(:md)
   end
 end
