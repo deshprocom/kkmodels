fba51051eb4d081b284015a48e470cef9fd137f6
Ricky <rickycxj@gmail.com>
2018-11-03 15:44:20 +0800

    用户奖品列表增加是否过期判断

--- wheel_user_prize.rb
+++ wheel_user_prize.rb
@@ -1,4 +1,9 @@
 class WheelUserPrize < ApplicationRecord
   belongs_to :user
   belongs_to :wheel_prize
+
+  def expired?
+    interval_day = (Time.zone.today - created_at.to_date).to_i
+    interval_day > 30
+  end
 end
