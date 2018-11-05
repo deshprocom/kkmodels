04e442b9e3d2fcba3ca7516492da488544ececb3
breeze-yang <breeze@deshpro.com>
2018-11-05 17:12:36 +0800

    增加 to_used

--- wheel_user_prize.rb
+++ wheel_user_prize.rb
@@ -6,4 +6,8 @@ class WheelUserPrize < ApplicationRecord
     interval_day = (Time.zone.today - created_at.to_date).to_i
     interval_day > 30
   end
+
+  def to_used
+    update(used: true, used_time: Time.current)
+  end
 end
