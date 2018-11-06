75f93bc04db6a864096a0882f85324a4799732df
Ricky <rickycxj@gmail.com>
2018-11-06 15:13:58 +0800

    添加奖品中奖说明

--- wheel_prize.rb
+++ wheel_prize.rb
@@ -6,4 +6,10 @@ class WheelPrize < ApplicationRecord
   # validates :wheel_element, presence: true
 
   enum prize_type: { cheap: 'cheap', expensive: 'expensive', free: 'free' }
+
+  def preview_image
+    return '' if image&.url.nil?
+
+    image.url(:sm)
+  end
 end
